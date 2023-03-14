71458c4b4a4048b4593c79506674d27787f3f764
Rizvan <rizvan@MacBook-Air.fritz.box>
2023-03-14 20:36:39 +0100

    todo-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,100 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Arrow from './resource/icons/Arrow.png';
 
 function App() {
+  const [todoList, setTodoList] = useState([
+    {
+      id: 1,
+      type: 'radio',
+      nameTodo: 'todo-list',
+      contain: ' Create a new todo...',
+    },
+    {
+      id: 2,
+      type: 'radio',
+      nameTodo: 'todo-list',
+      contain: ' Complete React project',
+    },
+    {
+      id: 3,
+      type: 'radio',
+      nameTodo: 'todo-list',
+      contain: ' Submit project',
+    },
+    {
+      id: 4,
+      type: 'radio',
+      nameTodo: 'todo-list',
+      contain: 'Read for 1 hour',
+    },
+    {
+      id: 5,
+      type: 'radio',
+      nameTodo: 'todo-list',
+      contain: ' Pick up groceries',
+    },
+  ]);
+  const [todo, setTodo] = useState('');
+
+  const addTodo: any = () => {
+    if (todo !== '') {
+      const newTodo = {
+        id: todoList.length + 1,
+        type: 'radio',
+        nameTodo: 'todo-list',
+        contain: todo,
+      };
+      setTodoList([...todoList, newTodo]);
+    }
+  };
+  const handleTodoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTodo(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>TODO</h1>
       </header>
+
+      <main>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+          }}
+          className="add-container"
+        >
+          <input
+            type="text"
+            placeholder="Create a new todo..."
+            id="add-todo"
+            onChange={handleTodoChange}
+          />
+          <button onClick={() => addTodo()}>
+            <img src={Arrow} alt="arrow-icon" />
+          </button>
+        </form>
+
+        <div className="todo-list">
+          <ul>
+            {todoList.map((todo) => (
+              <li key={todo.id}>
+                <input type={todo.type} name={todo.nameTodo} />
+                {todo.contain}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="result">
+          <div className="items-box">
+            <p>5</p>
+            <span>Items</span>
+          </div>
+          <div className="order-box">
+            <p>Clear Complete</p>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
